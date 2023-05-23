6e1e7ddd4f575fc749431684616594e846fa912c
帅帅 <renshuai0202@gmail.com>
2023-05-23 17:42:04 +0800

    feat: 统计1出现的次数

--- Array/01-reduce.js
+++ Array/01-reduce.js
@@ -6,3 +6,15 @@ const reducer = (total, currentValue, currentIndex, array) => {
 const res = arr.reduce(reducer, 0);
 
 console.log(res); // 75
+
+// 数组中元素出现的次数
+const arr2 = [1, 3, 5, 7, 9, 1];
+const show1Times = (total, currentValue, currentIndex, array) => {
+  if (currentValue === 1) {
+    total+=1;
+  }
+  return total;
+}
+const res2 = arr2.reduce(show1Times, 0);
+
+console.log(res2);
