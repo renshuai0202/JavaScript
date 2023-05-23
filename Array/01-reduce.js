f2cb897ac3df8344f23327d44f8b1d0aef505fa6
帅帅 <renshuai0202@gmail.com>
2023-05-23 18:10:45 +0800

    feat: reduce多维度统计

--- Array/01-reduce.js
+++ Array/01-reduce.js
@@ -20,3 +20,18 @@ const showTimes = (total, currentValue, currentIndex, array) => {
 const res2 = arr2.reduce(showTimes, {});
 
 console.log(res2);
+
+// 多维度数据统计
+const initialValue = { USD: 0, EURO: 0 };
+const USD = 0.1478;
+const EURO = 0.1265;
+const arr3 = [{ price: 10 }, { price: 50 }, { price: 100 }];
+
+const transform = (total, currentValue, currentIndex, array) => {
+  total.EURO += currentValue.price * EURO;
+  total.USD += currentValue.price * USD;
+  return total;
+}
+
+const res3 = arr3.reduce(transform, initialValue);
+console.log(res3);
