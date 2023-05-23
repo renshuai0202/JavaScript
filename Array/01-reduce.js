01c33bfd269c33026cd2bc1e48343f0e6978bb06
帅帅 <renshuai0202@gmail.com>
2023-05-23 17:51:26 +0800

    feat: 使用reduce，求每个元素出现的次数

--- Array/01-reduce.js
+++ Array/01-reduce.js
@@ -7,14 +7,16 @@ const res = arr.reduce(reducer, 0);
 
 console.log(res); // 75
 
-// 数组中元素出现的次数
+// 数组中每个元素出现的次数
 const arr2 = [1, 3, 5, 7, 9, 1];
-const show1Times = (total, currentValue, currentIndex, array) => {
-  if (currentValue === 1) {
-    total+=1;
+const showTimes = (total, currentValue, currentIndex, array) => {
+  if (total[currentValue] === undefined) {
+    total[currentValue] = 1;
+  } else {
+    total[currentValue]+=1;
   }
   return total;
 }
-const res2 = arr2.reduce(show1Times, 0);
+const res2 = arr2.reduce(showTimes, {});
 
 console.log(res2);
