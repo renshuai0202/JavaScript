3ba5aae2d3f2f29dda8e9afaef0f384073d88118
帅帅 <renshuai0202@gmail.com>
2023-05-23 18:28:11 +0800

    feat: reduce求最大值和最小值

--- Array/01-reduce.js
+++ Array/01-reduce.js
@@ -44,4 +44,20 @@ const res4 = arr4.reduce((total, currentValue, currentIndex, array) => {
 });
 console.log('res4', res4);
 
+
+// 求数组的最大值和最小值
+const arr5 = [2, 4, 10, 7, 5, 8, 6];
+const initialValue2 = { min: arr5[0], max: arr5[0] };
+
+const res5 = arr5.reduce((total, currentValue, currentIndex, array) => {
+  if (currentValue < total.min) {
+    total.min = currentValue;
+  } else if (currentValue > total.max) {
+    total.max = currentValue;
+  }
+  return total;
+}, initialValue2);
+
+console.log(res5);
+
 // 牢记，initialValue不只能是0，可以是对象，可以是任何类型
