9b1e8926ce81540ad59be7c85f4dae0038861a1e
帅帅 <renshuai0202@gmail.com>
2023-05-24 17:29:17 +0800

    feat: new

--- Object/02-new.js
+++ Object/02-new.js
@@ -10,10 +10,11 @@ console.log(foo);
 // 声明一个变量，赋值为空对象
 var foo2 = {};
 
-// 将变量的原型，指向构造函数的原型
+// 将变量的原型，指向构造函数的原型。
+// 继承构造函数的非静态方法和构造函数。
 foo2.__proto__ = Foo.prototype;
 
-// 呼叫转移，继承构造函数的非静态方法和构造函数。
+// 呼叫转移
 // 也可以理解为调用Foo
 // foo2.call(this, 'zhangsan', 18);
 Foo.call(foo2, 'zhangsan', 18);
